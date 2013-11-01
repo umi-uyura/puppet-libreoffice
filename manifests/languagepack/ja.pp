89f97c001b6f99b9ccf3961f322da558aec711b6
Umi Uyura <umi_uyura@nouveauvent.net>
2013-11-01 13:24:30 +0900

    Fix languagepack::ja not found

--- manifests/languagepack/ja.pp
+++ manifests/languagepack/ja.pp
@@ -7,6 +7,6 @@
 class libreoffice::languagepack::ja {
   package { 'LibreOffice LanguagePack for japanese':
     provider => 'appdmg',
-    source   => "http://download.documentfoundation.org/libreoffice/stable/4.1.2/mac/x86/LibreOffice_4.1.2_MacOS_x86_langpack_ja.dmg",
+    source   => "http://download.documentfoundation.org/libreoffice/stable/4.1.2/mac/x86/LibreOffice_4.1.2_MacOS_x86_langpack_ja.dmg"
   }
 }

a42ee7dbf7d3e7a44902287220350150f0d05087
Umi Uyura <umi_uyura@nouveauvent.net>
2014-04-01 16:42:25 +0900

    add support for languagepack version parameter

--- manifests/languagepack.pp
+++ manifests/languagepack.pp
@@ -8,10 +8,11 @@
 #
 #   class { 'libreoffice::languagepack':
 #     locale => 'de',
+#     version => '4.1.5
 #   }
-class libreoffice::languagepack ($locale = 'de') {
+class libreoffice::languagepack ($locale = 'de', $version='4.1.5') {
   package { 'LibreOffice LanguagePack':
     provider => 'appdmg',
-    source   => "http://download.documentfoundation.org/libreoffice/stable/4.1.4/mac/x86/LibreOffice_4.1.4_MacOS_x86_langpack_${locale}.dmg",
+    source   => "http://download.documentfoundation.org/libreoffice/stable/${version}/mac/x86/LibreOffice_${version}_MacOS_x86_langpack_${locale}.dmg",
   }
 }
